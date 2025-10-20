b799c031d239613f5b4c44d55b047df8f05e64c0
Amit Yadav <yadavamit0186@gmail.com>
2025-10-20 20:25:59 +0530

    Updated project with new changes

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "https://resume-backend-omega.vercel.app/api";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
 
 // const axiosInstance = axios.create({
 //   baseURL: API_BASE,

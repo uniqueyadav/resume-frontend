0e187cb80023cb5ddaa6e5d5b7b66a3699b68a27
Amit Yadav <yadavamit0186@gmail.com>
2025-10-20 20:53:27 +0530

    update url

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API_BASE = import.meta.env.VITE_API_BASE || "https://resume-backend-theta-indol.vercel.app/api";
 
 // const axiosInstance = axios.create({
 //   baseURL: API_BASE,

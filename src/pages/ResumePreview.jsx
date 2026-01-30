e5b2a9f6e1c33d95ac8ce6820e6f996d9959f7bf
Amit Yadav <yadavamit0186@gmail.com>
2026-01-31 01:02:17 +0530

    update

--- src/pages/ResumePreview.jsx
+++ src/pages/ResumePreview.jsx
@@ -140,9 +140,9 @@ export default function ResumePreview() {
         </footer>
       </div>
        {/* Footer */}
-      <footer className="login-footer">
-        © 2025 Designed By <strong>AMIT YADAV</strong> | All Rights Reserved
-      </footer>
+      {/* <footer className="login-footer">
+        © 2025 Designed By AMIT YADAV | All Rights Reserved
+      </footer> */}
     </div>
   );
 }
